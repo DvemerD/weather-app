1f4dd43c67f774e073193f32f11549f6d73b2028
Danil_Rybkin <danil.rybkin.19@gmail.com>
2022-08-03 11:23:50 +0300

    preparation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const config = require('config');
+const path = require('path');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -7,6 +8,14 @@ const app = express();
 app.use(express.json());
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/cities', require('./routes/city.routes'));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+
+    app.get('*', () => {
+        resizeBy.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = config.get('port') || 5000;
 
